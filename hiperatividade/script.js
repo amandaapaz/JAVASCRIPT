a0ff384c748a9369f09eda9e1fe3e82e061ca7b7
amandaapaz <123338590+gaiabank@users.noreply.github.com>
2023-03-04 20:27:20 -0300

    funcional
    1 step

--- hiperatividade/script.js
+++ hiperatividade/script.js
@@ -1,32 +1,57 @@
+const form = document.querySelector('#quiz-form');
+const progress = document.querySelector('#progress-bar');
 const steps = document.querySelectorAll('.step');
+const resultButton = document.querySelector('.result-button');
 
+let currentStep = 0;
+
+// Função para atualizar a barra de progresso
+function updateProgress() {
+  const progressValue = ((currentStep + 1) / steps.length) * 100;
+  progress.value = progressValue;
+}
+
+// Função para mostrar o passo atual
+function showCurrentStep() {
+  steps.forEach((step, index) => {
+    if (index === currentStep) {
+      step.classList.add('active');
+    } else {
+      step.classList.remove('active');
+    }
+  });
+}
+
+// Função para avançar para o próximo passo
 function nextStep() {
-  const currentStep = document.querySelector('.active');
-  const currentStepIndex = Array.from(steps).indexOf(currentStep);
-  const nextStepIndex = currentStepIndex + 1;
-
-  if (nextStepIndex < steps.length) {
-    currentStep.classList.remove('active');
-    steps[nextStepIndex].classList.add('active');
-    updateProgressBar();
+  currentStep++;
+  if (currentStep >= steps.length) {
+    form.submit();
+  } else {
+    updateProgress();
+    showCurrentStep();
   }
 }
 
-function updateProgressBar() {
-  const currentStep = document.querySelector('.active');
-  const currentStepIndex = Array.from(steps).indexOf(currentStep);
-  const progressBar = document.querySelector('#progress-bar');
-  progressBar.value = ((currentStepIndex + 1) / steps.length) * 100;
-}
-
-function submitForm() {
-  const form = document.querySelector('#quiz-form');
-  form.submit();
-}
-
-steps.forEach((step) => {
-  const inputs = step.querySelectorAll('input[type="radio"]');
-  inputs.forEach((input) => {
-    input.addEventListener('click', nextStep);
-  });
+// Adicionar o evento click para cada botão de resposta
+const answerButtons = document.querySelectorAll('input[type="radio"]');
+answerButtons.forEach((button) => {
+  button.addEventListener('click', nextStep);
 });
+
+// Esconder todos os passos, exceto o primeiro
+steps.forEach((step, index) => {
+  if (index !== 0) {
+    step.classList.remove('active');
+  }
+});
+
+// Adicionar o evento de envio do formulário para o botão de resultado
+resultButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  form.submit();
+});
+
+// Iniciar o progresso e mostrar o primeiro passo
+updateProgress();
+showCurrentStep();
