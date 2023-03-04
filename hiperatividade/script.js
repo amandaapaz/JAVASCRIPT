abdef79f43f42fd42b3de0622ca37654225af194
amandaapaz <123338590+gaiabank@users.noreply.github.com>
2023-03-04 18:37:58 -0300

    todas as respostas
    new

--- hiperatividade/script.js
+++ hiperatividade/script.js
@@ -1,32 +1,39 @@
-const steps = document.querySelectorAll('.step');
+const form = document.querySelector('#quiz-form');
+const steps = form.querySelectorAll('.step');
+const progressBar = document.querySelector('#progress-bar');
+let currentStep = 0;
+
+function showStep(stepNumber) {
+  steps[currentStep].classList.remove('active');
+  steps[stepNumber].classList.add('active');
+  progressBar.value = (stepNumber + 1) / steps.length * 100;
+  currentStep = stepNumber;
+}
 
 function nextStep() {
-  const currentStep = document.querySelector('.active');
-  const currentStepIndex = Array.from(steps).indexOf(currentStep);
-  const nextStepIndex = currentStepIndex + 1;
-
-  if (nextStepIndex < steps.length) {
-    currentStep.classList.remove('active');
-    steps[nextStepIndex].classList.add('active');
-    updateProgressBar();
+  const currentFieldset = steps[currentStep].querySelector('fieldset');
+  const selectedInput = currentFieldset.querySelector('input:checked');
+  
+  if (!selectedInput) {
+    alert('Por favor, selecione uma opção.');
+    return;
   }
+  
+  if (currentStep === steps.length - 1) {
+    form.submit();
+    return;
+  }
+  
+  showStep(currentStep + 1);
 }
 
-function updateProgressBar() {
-  const currentStep = document.querySelector('.active');
-  const currentStepIndex = Array.from(steps).indexOf(currentStep);
-  const progressBar = document.querySelector('#progress-bar');
-  progressBar.value = ((currentStepIndex + 1) / steps.length) * 100;
-}
-
-function submitForm() {
-  const form = document.querySelector('#quiz-form');
-  form.submit();
-}
-
-steps.forEach((step) => {
-  const inputs = step.querySelectorAll('input[type="radio"]');
-  inputs.forEach((input) => {
+steps.forEach((step, index) => {
+  const fieldset = step.querySelector('fieldset');
+  const inputs = fieldset.querySelectorAll('input');
+  
+  inputs.forEach(input => {
     input.addEventListener('click', nextStep);
   });
 });
+
+showStep(0);
