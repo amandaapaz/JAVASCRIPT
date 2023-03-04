90c3179553b49bfaf5ceaf30e5693e5fd798d45c
amandaapaz <123338590+gaiabank@users.noreply.github.com>
2023-03-04 18:41:58 -0300

    js
    nova versão

--- hiperatividade/script.js
+++ hiperatividade/script.js
@@ -1,33 +1,57 @@
-// Seleciona os elementos do formulário e inicializa o valor do contador de progresso
-const form = document.getElementById('quiz-form');
-const steps = form.querySelectorAll('.step');
-const progressBar = document.getElementById('progress-bar');
-let progress = 0;
+const form = document.querySelector('#quiz-form');
+const progress = document.querySelector('#progress-bar');
+const steps = document.querySelectorAll('.step');
+const resultButton = document.querySelector('.result-button');
 
-// Esconde todos os passos, exceto o primeiro
-for (let i = 1; i < steps.length; i++) {
-  steps[i].classList.remove('active');
+let currentStep = 0;
+
+// Função para atualizar a barra de progresso
+function updateProgress() {
+  const progressValue = ((currentStep + 1) / steps.length) * 100;
+  progress.value = progressValue;
 }
 
-// Adiciona um manipulador de eventos de clique para cada botão de resposta
-const answerButtons = form.querySelectorAll('input[type="radio"]');
-answerButtons.forEach(button => {
-  button.addEventListener('click', event => {
-    event.preventDefault();
-    // Obtém o passo atual e próximo
-    const currentStep = form.querySelector(`.step[data-step="${form.dataset.activeStep}"]`);
-    const nextStep = form.querySelector(`.step[data-step="${parseInt(form.dataset.activeStep) + 1}"]`);
-    // Atualiza o valor do contador de progresso e a barra de progresso
-    progress += 100 / steps.length;
-    progressBar.value = progress;
-    // Verifica se há um próximo passo e o torna ativo se houver
-    if (nextStep) {
-      currentStep.classList.remove('active');
-      nextStep.classList.add('active');
-      form.dataset.activeStep = nextStep.dataset.step;
+// Função para mostrar o passo atual
+function showCurrentStep() {
+  steps.forEach((step, index) => {
+    if (index === currentStep) {
+      step.classList.add('active');
     } else {
-      // Submete o formulário se estiver na última pergunta
-      form.submit();
+      step.classList.remove('active');
     }
   });
+}
+
+// Função para avançar para o próximo passo
+function nextStep() {
+  currentStep++;
+  if (currentStep >= steps.length) {
+    form.submit();
+  } else {
+    updateProgress();
+    showCurrentStep();
+  }
+}
+
+// Adicionar o evento click para cada botão de resposta
+const answerButtons = document.querySelectorAll('input[type="radio"]');
+answerButtons.forEach((button) => {
+  button.addEventListener('click', nextStep);
 });
+
+// Esconder todos os passos, exceto o primeiro
+steps.forEach((step, index) => {
+  if (index !== 0) {
+    step.classList.remove('active');
+  }
+});
+
+// Adicionar o evento de envio do formulário para o botão de resultado
+resultButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  form.submit();
+});
+
+// Iniciar o progresso e mostrar o primeiro passo
+updateProgress();
+showCurrentStep();
